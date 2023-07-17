133e124b3edaaa6d63c79907485f6d5eb7672d8f
MarioMartinez00072520 <00072520@uca.edu.sv>
2023-07-16 21:08:41 -0600

    Add: react routes at app jsx

--- uca-hub-landing-page/src/App.jsx
+++ uca-hub-landing-page/src/App.jsx
@@ -2,16 +2,21 @@ import style from "./App.module.scss"
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer"
 import Landing from "./components/Landing/Landing";
-
+import { Routes, Route } from "react-router-dom";
 function App() {
 
 
   return (
     <>
       <Header />
-      <main className={style["main-content"]}>
-        <Landing/>
-      </main>
+      <Routes>
+        <Route path="/home" element={
+          <main className={style["main-content"]}>
+            <Landing/>
+          </main>
+        }/>
+
+      </Routes>
       <Footer />
     </>
   )
